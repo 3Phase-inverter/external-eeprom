2c7133cea586116c5de14256cb6fa19165abe5f0
soheilpaper <soheil_paper@yahoo.com>
2016-04-29 15:35:10 +0430

    first commit

---
+++ GccApplication2/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+EEPROM_routines.c
+
+GccApplication2.c
+
+i2c_routines.c
+
+RTC_routines.c
+
+UART_routines.c
+
